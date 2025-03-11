28b8a3c2a5d3520e04809f6ab84ccf98211f138d
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 07:07:50 +0700

    KODE DENGAN TEKNIK TABLE DRIVEN

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("");
+﻿using System;
+using System.Collections.Generic;
+
+public class KodeProduk
+{
+    private static Dictionary<string, string> kodeProdukTable = new Dictionary<string, string>
+    {
+        { "Laptop", "E100" },
+        { "Smartphone", "E101" },
+        { "Tablet", "E102" },
+        { "Headset", "E103" },
+        { "Keyboard", "E104" },
+        { "Mouse", "E105" },
+        { "Printer", "E106" },
+        { "Monitor", "E107" },
+        { "Smartwatch0", "E108" },
+        { "Kamera", "E109" }
+    };
+
+public static string GetKodeProduk(string elektronik)
+{
+    if (kodeProdukTable.ContainsKey(elektronik))
+    {
+        return kodeProdukTable[elektronik];
+    }
+    else
+    {
+        return "Kode produk tidak ditemukan";
+    }
+}
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Masukkan nama barang elektronik: ");
+        string elektronik = Console.ReadLine();
+
+        string kodeProduk = KodeProduk.GetKodeProduk(elektronik);
+
+        Console.WriteLine($"Kode Produk untuk {elektronik} adalah {kodeProduk}");
+    }
+}
